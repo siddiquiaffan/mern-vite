fbd79b3e94f441dd6eded39d9a05d1d8aa7a180a
siddiquiaffan <siddiquiaffan201@gmail.com>
2023-07-19 13:54:00 +0530

    Fix type

--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -3,7 +3,7 @@ import express, { Response, Request } from 'express'
 
 const router = express.Router()
 
-router.get('/data', (req: Response, res: Request) => {
+router.get('/data', (req: Request, res: Response) => {
     // logic to fetch data
     const data = {
         name: 'John Doe'
